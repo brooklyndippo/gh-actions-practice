afe6cfcbe46ab2b398227f93fff3ee973c1bef03
Mitchell <soggybag@gmail.com>
2022-02-23 07:27:58 -0800

    added doMath

--- src/index.js
+++ src/index.js
@@ -2,4 +2,6 @@ export function hello() {
 	return 'Hello World'
 }
 
-
+export function doMath(a, b) {
+	return a * b
+}
--- tests/index.test.js
+++ tests/index.test.js
@@ -1,6 +1,10 @@
 import {jest} from '@jest/globals';
-import { hello } from '../src/index.js'
+import { hello, doMath } from '../src/index.js'
 
 test('Test Hello', () => {
 	expect(hello()).toBe('Hello World')
+})
+
+test('Do math', () => {
+	expect(doMath(2, 4)).toBe(8)
 })
